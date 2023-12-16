bb9ffa120c283153566db1517009103cad5a83ef
Abimelec <abimelecg2@unitec.edu>
2023-12-16 08:43:19 -0600

    intent3

--- lib/services/book_service.dart
+++ lib/services/book_service.dart
@@ -3,11 +3,11 @@ import 'package:flutter_examen/models/book.dart';
 
 class BookService {
   final Dio _dio = Dio();
-  final String _baseUrl = 'https://stephen-king-api.onrender.com/api/books';
+  final String _baseUrl = 'https://stephen-king-api.onrender.com';
 
   Future<List<Book>> getBooks() async {
     try {
-      final response = await _dio.get('$_baseUrl/books');
+      final response = await _dio.get('https://stephen-king-api.onrender.com/api/books');
       return (response.data as List).map((book) => Book.fromJson(book)).toList();
     } catch (e) {
       throw Exception('Error fetching books: $e');
